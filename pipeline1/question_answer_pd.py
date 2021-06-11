31edad867b75db0867047a643b3cf81880e26e65
Ayush Maheshwari <69768815+am11917@users.noreply.github.com>
2021-06-11 16:53:18 -0400

    Update and rename test.py to question_answer_pd.py

---
+++ pipeline1/question_answer_pd.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import os
+from transformers.pipelines import pipeline
+
+# function to read file from input directory
+# answer the questions and write it to output directory
+def question_answer(qa_file):
+    #importing the file using pandas library
+    data = pd.read_csv(qa_file)
+   
+    hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad")
+    answer = []
+    score = []
+    questions = []
+    for idx, row in data.iterrows():
+        context = row['context']
+        question = row['question']
+        questions.append(question)
+        answer.append(hg_comp({'question': question, 'context': context})['answer'])
+        score.append(hg_comp({'question': question, 'context': context})['score'])
+    
+    data["answer"] = answer
+    data.to_csv('/pfs/output/answers/'+qa_file, index=False)    
+
+    
+# walk /pfs/question_answer and call question_answer on every file found
+for dirpath, dirs, files in os.walk("/pfs/question_answer"):
+   for file in files:
+       question_answer(os.path.join(dirpath, file))
