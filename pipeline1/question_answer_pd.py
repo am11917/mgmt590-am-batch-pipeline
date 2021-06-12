5c2ae64d76d209eb0e0da5e79f6f8d9ebd29e7bb
Ayush Maheshwari <69768815+am11917@users.noreply.github.com>
2021-06-11 20:58:54 -0400

    Update question_answer_pd.py

--- pipeline1/question_answer_pd.py
+++ pipeline1/question_answer_pd.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import csv
 import os
 import time
 from transformers.pipelines import pipeline
@@ -7,22 +7,40 @@ from transformers.pipelines import pipeline
 # answer the questions and write it to output directory
 def question_answer(qa_file):
     #importing the file using pandas library
-    data = pd.read_csv(qa_file)
-   
+    #data = pd.read_csv(qa_file)
+    final_answer = []
+       
     hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad")
     answer = []
-    score = []
     questions = []
-    for idx, row in data.iterrows():
-        context = row['context']
-        question = row['question']
-        questions.append(question)
-        answer.append(hg_comp({'question': question, 'context': context})['answer'])
-        score.append(hg_comp({'question': question, 'context': context})['score'])
+    contexts = []
     
-    data["answer"] = answer
+    with open(qa_file,'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            context = row['context']
+            question = row['question']
+            answer.append(hg_comp({'question': question, 'context': context})['answer'])
+            questions.append(question)
+            contexts.append(context)
+        final_answer.append(questions)
+        final_answer.append(contexts)
+        final_answer.append(answer)
+        print (final_answer)
+    #for idx, row in data.iterrows():
+    #    context = row['context']
+    #    question = row['question']
+    #    questions.append(question)
+    #    answer.append(hg_comp({'question': question, 'context': context})['answer'])
     timestamp = str(int(time.time()))
-    data.to_csv("/pfs/out/"+"question_answer"+timestamp+".csv", index=False)    
+    with open('/pfs/out/'+"question_answer_"+timestamp+".csv","wb") as f:
+        fileWriter = csv.writer(f, delimiter=',')
+        for row in zip(*final_answer):
+            fileWriter.writerow(row)
+ 
+    
+    #data["answer"] = answer
+    #data.to_csv("/pfs/out/"+"question_answer"+timestamp+".csv", index=False)    
 
     
 # walk /pfs/question_answer and call question_answer on every file found
