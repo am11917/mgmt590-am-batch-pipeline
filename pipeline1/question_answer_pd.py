db14d9fcdce15ede1edd1dbe38d33f21fd87849b
Ayush Maheshwari <69768815+am11917@users.noreply.github.com>
2021-06-12 02:23:47 -0400

    Update question_answer_pd.py

--- pipeline1/question_answer_pd.py
+++ pipeline1/question_answer_pd.py
@@ -1,84 +1,34 @@
-import csv
 import os
-import time
-import logging
-from google.cloud import storage
+import io
+from io import BytesIO
 from transformers.pipelines import pipeline
+from google.cloud import storage
+import pandas as pd
 
-# function to read file from input directory
-# answer the questions and write it to output directory
-def question_answer(qa_file):
-    #importing the file using pandas library
-    final_answer = []    
-    hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad")
-    answer = []
-    questions = []
-    contexts = []
-    
-    with open(qa_file,'r') as file:
-        print("inside file reading loop")
-        reader = csv.DictReader(file)
-        print("file read complete")
-        print(reader)
-        for row in reader:
-            print("Inside File Row Read loop")
-            print(row)
-            context = row["context"]
-            question = row["question"]
-            answer.append(hg_comp({'question': question, 'context': context})['answer'])
-            questions.append(question)
-            contexts.append(context)
-        final_answer.append(questions)
-        final_answer.append(contexts)
-        final_answer.append(answer)
-        print (final_answer)
+hg_comp = pipeline('question-answering', model="distilbert-base-uncased-distilled-squad", tokenizer="distilbert-base-uncased-distilled-squad")
 
-    timestamp = str(int(time.time()))
-    with open('/pfs/out/'+"question_answer_"+timestamp+".csv",'w') as f:
-        fileWriter = csv.writer(f, delimiter=',')
-        print("Inside the file write block")
-        for row in zip(*final_answer):
-            fileWriter.writerow(row)
-    print("File Write Complete")
- 
-def downloadFiles():
-    logging.debug('Inside Download Files')
-    print("Inside Download Files")
-    try:
-        blobs = bucket.list_blobs()
-        print("Read the list of blobs")
-        for blob in blobs:
-            print("Inside the blob loop")
-            blob.download_to_filename('/pfs/question/'+blob.name)
-    except Exception as ex:
-       logging.error("Exception occurred while trying to download files " , ex)
+storage_client = storage.Client()
+bucket = storage_client.get_bucket('mgmt590-prd-file-upload')
 
-def delete_one_file(filename):
-    logging.debug('Inside delete files') 
-    print("Inside the delete file function")
-    try:
-        blob = bucket.blob(filename)
-        print("retriveing file name and already put in the blob")
-        blob.delete()
-        print("delete complete")
-    except Exception as ex:
-        logging.error("Exception occurred while trying to delete files ",ex)
-    
-if __name__ == '__main__':
-    storage_client = storage.Client()
-    bucket = storage_client.get_bucket('mgmt590-prd-file-upload')
-    print("Downloading Files")
-    downloadFiles()
-    
-    # walk /pfs/question_answer and call question_answer on every file found
-    for dirpath, dirs, files in os.walk("/pfs/question"):
-        for file in files:
-            print("We are looping in the files")
-            print("File Name: "+file)
-            print(os.path.join(dirpath,file))
-            question_answer(os.path.join(dirpath,file))
-            print("Questions Answered and File Successfully Written")
-            print("Initiating the Delete method to delete file from GCS")
-            delete_one_file(file)
-            print("File Deleted Successfully from GCS")
-    
+files = bucket.list_blobs()
+fileList = [file.name for file in files if '.' in file.name]
+
+answer_intmd = []
+df_intmd = pd.DataFrame()
+df_final = pd.DataFrame()
+for fileName in fileList:
+    am_blob = bucket.blob(fileName)
+    data = am_blob.download_as_string()
+    df = pd.read_csv(io.BytesIO(data), encoding = 'utf-8',sep = ',')
+    df_intmd = df_intmd.append(df,ignore_index = True)
+    bucket.delete_blob(fileName)
+    for fileName,row in df.iterrows():
+        context = row['context']
+        question = row['question']
+        answer = hg_comp({'question': question, 'context': context})['answer']
+        answer_intmd.append(answer)
+#print(answer_li)
+df_final['context'] = df_intmd['context']
+df_final['question'] = df_intmd['question']
+df_final['answer'] = answer_intmd
+df_final.to_csv("pfs/out/answers.csv", index=False, header=True)
